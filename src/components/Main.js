dfda84c94e8627a1469d7ceb5f2de56949278d6f
ByronGBP <byrongbp@gmail.com>
2018-09-02 17:33:23 +0200

    header styles

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+// @flow
+import React, { Component } from 'react';
+
+import Header from './Header/Header';
+
+type Props = {};
+
+class Main extends Component<Props> {
+  render () {
+    return (
+      <Header/>
+    );
+  }
+}
+
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 // @flow
-// $FlowFixMe
-import './styles/main';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import Main from './components/Main';
 
-const title: string = 'YOUH!';
 const container: HTMLElement | null = document.getElementById('main-container');
 
 if (!container) {
   throw new Error(`Container doesn't exist`);
 }
 
-ReactDOM.render(<App title={title}/>, container);
+ReactDOM.render(<Main/>, container);
