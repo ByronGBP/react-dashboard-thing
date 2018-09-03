87c9905c9af8fd810d07e685efa968cbb86f26f2
ByronGBP <byrongbp@gmail.com>
2018-09-03 22:28:23 +0200

    reposition

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,16 +4,16 @@ import React from 'react';
 const Header = () => (
   <div className="container">
     <div className="row hld-header">
-      <div className="col-4 d-flex justify-content-center flex-column">
-        <div className="nvx-w-min-content p-4">
+      <div className="col-5 d-flex justify-content-center flex-column">
+        <div className="nvx-w-min-content px-2 py-4">
           <span className="d-block hld-icon-set hld-menu-hambuger"></span>
         </div>
       </div>
-      <div className="col-4 d-flex justify-content-center align-items-center">
+      <div className="col-2 d-flex justify-content-center align-items-center">
         <img className="hld-img-logo" src="https://app.holded.com/assets/img/hround.png" alt="hld logo"/>
       </div>
-      <div className="col-4 d-flex justify-content-center align-items-center nvx-color-black">
-        <div className="w-100 d-flex justify-content-between">
+      <div className="col-5 d-flex align-items-center nvx-color-black">
+        <div className="w-100 d-flex justify-content-around">
           <i className="fas fa-plus-circle fs-18"></i>
           <i className="fas fa-cog fs-18"></i>
           <i className="fas fa-briefcase fs-18"></i>
